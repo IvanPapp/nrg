5509e380035d0a5a06906dd91fc29a3677ad4d94
IvanPapp <115174950+IvanPapp@users.noreply.github.com>
2023-01-17 22:03:15 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+function loadPage(){
+    window.open("./hamburger.html", "_self")
+}
+
+
+/* function redirect_Page () {
+    var tID = setTimeout(function () {
+        window.location.href = "./hamburger.html";
+        window.clearTimeout(tID);		// clear time out.
+    }, 4000);
+} */
